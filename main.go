4b1ca6f5fe851c598a39fa4cee93af975b188ff8
Your Name <you@example.com>
2024-03-22 18:47:48 +0500

    cors

--- main.go
+++ main.go
@@ -16,4 +16,18 @@ func main() {
 	r.GET("/allmusic", controllers.AllMusics)
 
 	r.Run(":3030")
-}
+	
+	}
+	
+	func Cors(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://192.168.43.246:5500")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		}
+	
+		c.Next()
+	}
+	
